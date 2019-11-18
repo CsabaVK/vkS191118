c3f8a5a1cec0f081acf6129d6aca483aacf57494
Csaba <56678429+CsabaVK@users.noreply.github.com>
2019-11-18 11:31:11 +0100

    Add files via upload

---
+++ cucc.sql
@@ -0,0 +1,25 @@
+-- Keressük meg a medelszámát... GECI GYORSAN GÖRGETSZ BAZDMEG
+SELECT
+modell,
+sebesseg,
+merevlemez AS 'merevlemez-kapacitás'
+WHERE ar < 1000;
+
+
+-- Keressük meg a nyomtatók gyártóit!
+SELECT 
+	gyarto -- gyarto --> * (SELECT * FROM)
+	FROM `&_termekek`
+	WHERE tipus = 'nyomtato'
+	GROUP BY gyarto
+	
+	
+-- 
+Keressük meg azon laptopok modellszámát, memóriakapacitását 
+-- és képernyőnagyságát, melyek több mint $1500-ba kerülnek
+SELECT 
+	Laptop_id,
+	sebesseg,
+	kijelzo_meret
+	FROM `&_laptopok`
+	WHERE ar > 1500;
